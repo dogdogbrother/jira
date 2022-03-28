922f0a3248af03a52222a2ebee0e32ce8f412e82
森林 <gougewh@sina.com>
2022-03-28 14:01:17 +0800

    feat: 完善登录鉴权

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,7 +2,7 @@ import { SearchPanel } from './search-panel'
 import { List } from './list'
 import { useEffect, useState } from 'react'
 import { cleanObject, useDebounce, useMount } from '../../utils'
-import * as qs from 'qs'
+import { useHttp } from 'utils/http'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -15,15 +15,9 @@ export const ProjectListScreen = () => {
   })
   const debouncedParam = useDebounce(param, 500)
   const [list, setList] = useState([])
-
+  const client = useHttp()
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json())
-      }
-    })
+    client('projects', { data: cleanObject(debouncedParam) }).then(setList)
   }, [debouncedParam])
 
   useMount(() => {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,27 +1,29 @@
 import { useEffect, useState } from 'react'
 
-export const isFalsy = (value) => (value === 0 ? false : !value)
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value)
 
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object }
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key]
     // 排除 非0 的空值
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key]
     }
   })
   return result
 }
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback()
   }, [])
 }
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = <V>(value: V, delay: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
